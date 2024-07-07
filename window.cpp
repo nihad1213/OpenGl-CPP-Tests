a7792efcf731a72d452117b96a25ccf5a11779ed
nihad1213 <nihad.nemetli@gmail.com>
2024-07-07 21:13:49 +0400

    GLAD glm donwloaded

--- window.cpp
+++ window.cpp
@@ -1,29 +1,33 @@
+#include <glad/glad.h>
 #include <GLFW/glfw3.h>
+#include <glm/mat4x4.hpp>
 #include <iostream>
 
 int main() {
-
     if (!glfwInit())
         return -1;
-    
+
     GLFWwindow *window = glfwCreateWindow(800, 600, "Test", NULL, NULL);
 
+    glm::mat4 mat4;
 
     if (window == NULL) {
         std::cout << "Failed To Create Window";
-
         glfwTerminate();
-
         return -1;
     }
 
     glfwMakeContextCurrent(window);
     glfwSwapInterval(1);
 
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+        std::cout << "Failed to initialize GLAD";
+        return -1;
+    }
+
     while (!glfwWindowShouldClose(window)) {
         glClear(GL_COLOR_BUFFER_BIT);
         glfwSwapBuffers(window);
-        
         glfwPollEvents();
     }
 
